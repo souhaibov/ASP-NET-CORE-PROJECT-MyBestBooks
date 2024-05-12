03ee05585d1ff3149aa8e9d5b6408b0760d1d847
souhaibovsky <souhaibovsky@SOUHAIBOV>
2024-05-12 16:57:51 +0100

    Add project files.

---
+++ MyBestMovies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyBestMovies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
